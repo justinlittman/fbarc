cc806170adc5cda9afc9772a237913b1bb9bf165
Justin Littman <justinlittman@gwu.edu>
2017-12-24 15:27:10 -0500

    Removed post fields that were causing errors.

--- definitions/post.py
+++ definitions/post.py
@@ -8,22 +8,26 @@ definition = {
         'attachments': {'omit': True},
         'backdated_time': {'omit': True},
         'call_to_action': {'omit': True},
-        'caption': {},
+        # Removed since causes error
+        'caption': {'omit': True},
         'child_attachments': {'omit': True},
         'comments': {'edge_type': 'comment'},
         'comments_mirroring_domain': {'omit': True},
         'coordinates': {'omit': True},
         'created_time': {'default': True},
-        'description': {},
+        # Removed since causes error
+        'description': {'omit': True},
         'dynamic_posts': {'omit': True},
         'event': {'edge_type': 'event', 'follow_edge': False},
         'expanded_height': {'omit': True},
         'expanded_width': {'omit': True},
         'feed_targeting': {'omit': True},
         'from': {'default': True},
-        'full_picture': {},
+        # Removed since causes error
+        'full_picture': {'omit': True},
         'height': {'omit': True},
-        'icon': {},
+        # Removed since causes error
+        'icon': {'omit': True},
         'insights': {'omit': True},
         'instagram_eligibility': {'omit': True},
         'is_app_share': {'omit': True},
@@ -34,28 +38,34 @@ definition = {
         'is_published': {'omit': True},
         'is_spherical': {'omit': True},
         'likes': {},
-        # link and type cause an error for some posts when getting edges
-        'link': {},
+        # Removed since causes error
+        'link': {'omit': True},
         'message': {'default': True},
         'message_tags': {},
         'multi_share_end_card': {'omit': True},
         'multi_share_optimized': {'omit': True},
-        'name': {},
-        'object_id': {},
-        'parent_id': {},
+        # Removed since causes error
+        'name': {'omit': True},
+        # Removed since causes error
+        'object_id': {'omit': True},
+        # Removed since causes error
+        'parent_id': {'omit': True},
         'permalink_url': {'default': True},
-        'picture': {},
+        # Removed since causes error
+        'picture': {'omit': True},
         'place': {},
         'privacy': {'omit': True},
         'promotion_status': {'omit': True},
-        'properties': {},
+        # Removed since causes error
+        'properties': {'omit': True},
         'reactions': {},
         'scheduled_publish_time': {'omit': True},
         'seen': {'omit': True},
         'sharedposts': {'omit': True},
         # For some reason, shares are causing (#2) Service temporarily unavailable on some posts.
         'shares': {'omit': True},
-        'source': {},
+        # Removed since causes error
+        'source': {'omit': True},
         'sponsor_tags': {'omit': True},
         'status_type': {'default': True},
         'story': {},
@@ -65,8 +75,8 @@ definition = {
         'targeting': {'omit': True},
         'timeline_visibility': {'omit': True},
         'to': {'default': True},
-        # link and type cause an error for some posts when getting edges
-        'type': {},
+        # Removed since causes error
+        'type': {'omit': True},
         'updated_time': {'default': True},
         'via': {},
         'video_buying_eligibility': {'omit': True},
