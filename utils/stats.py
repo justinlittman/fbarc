7566ed8e9bf8400893b3b7aeea1cec337fde5644
Justin Littman <justinlittman@gwu.edu>
2017-12-31 19:09:33 -0500

    Added stats util.

---
+++ utils/stats.py
@@ -0,0 +1,23 @@
+import fileinput
+import json
+from collections import Counter
+import sys
+
+"""
+Counts the node types provided in a list of JSON files or stdin.
+"""
+
+if __name__ == '__main__':
+    stats_counter = Counter()
+
+    for line in fileinput.input():
+        node = json.loads(line)
+        if 'id' in node:
+            if 'metadata' in node:
+                stats_counter[node['metadata']['type']] += 1
+        else:
+            print('Error line: {}'.format(line))
+            sys.exit(1)
+
+    for node_type, count in stats_counter.most_common():
+        print('{}: {}'.format(node_type, count))
